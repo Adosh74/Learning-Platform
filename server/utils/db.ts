df146acc5a0f1bc3333dba1c9831f102997c8dfb
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-27 09:31:53 +0300

    connection with mongo db

---
+++ server/utils/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+	try {
+		if (!process.env.MONGO_URI) throw new Error('MONGO_URI is not defined');
+		mongoose.connect(process.env.MONGO_URI).then(() => {
+			console.log(
+				`MongoDB connected successfully with ${process.env.MONGO_DB?.toUpperCase()}!`
+			);
+		});
+	} catch (error) {
+		console.log(error);
+		setTimeout(connectDB, 5000);
+	}
+};
+
+export default connectDB;
