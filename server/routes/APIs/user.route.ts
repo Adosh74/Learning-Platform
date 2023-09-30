8e956f832f607bd448aca5fbbd151e688deddb03
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-30 12:22:23 +0300

    create login and logout routes

--- server/routes/APIs/user.route.ts
+++ server/routes/APIs/user.route.ts
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post('/register', userControllers.register);
 router.post('/activate-user', userControllers.activateUser);
+router.post('/login', userControllers.login);
+router.get('/logout', userControllers.logout);
 
 export default router;
