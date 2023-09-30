94be3c80b6416fbdabed1a6826257b33cc027b41
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-30 12:22:40 +0300

    update redis connection

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,7 +1,14 @@
+import { config } from 'dotenv';
 import { Redis } from 'ioredis';
 
+config({ path: './.env' });
+
 const redisClient = () => {
-	if (!process.env.REDIS_URI) throw new Error('REDIS_URI is not defined');
+	if (!process.env.REDIS_URI) {
+		throw new Error('REDIS_URI is not defined');
+	}
+	console.log('redis connected successfully!');
+
 	return process.env.REDIS_URI;
 };
 export const redis = new Redis(redisClient());
