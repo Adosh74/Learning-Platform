3553288c9b1d9da365cf4711479803726bd2d122
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-05 15:57:46 +0300

    add refresh route

--- server/routes/APIs/user.route.ts
+++ server/routes/APIs/user.route.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import * as userControllers from '../../controllers/user.controller';
+import * as authControllers from '../../controllers/auth.controller';
 import { isAuthenticated } from '../../middleware/auth';
 
 const router = Router();
 
-router.post('/register', userControllers.register);
-router.post('/activate-user', userControllers.activateUser);
-router.post('/login', userControllers.login);
-router.get('/logout', isAuthenticated, userControllers.logout);
+router.post('/register', authControllers.register);
+router.post('/activate-user', authControllers.activateUser);
+router.post('/login', authControllers.login);
+router.get('/logout', isAuthenticated, authControllers.logout);
+
+router.get('/refresh', authControllers.updateAccessToken);
 
 export default router;
