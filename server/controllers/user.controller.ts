563b51108698cd4c4f6894c9a7c66c9111c95627
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-21 22:40:41 +0300

    add update profile picture function

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import cloudinary from 'cloudinary';
 import { NextFunction, Request, Response } from 'express';
 import User, { IUser } from '../models/user.model';
 import { getUserById } from '../services/user.service';
@@ -42,6 +43,51 @@ export const updateUserInfo = catchAsync(
 		// save user and update cache
 		await user.save();
 
+		await redis.set(userId || '', JSON.stringify(user));
+
+		res.status(200).json({
+			success: true,
+			user,
+		});
+	}
+);
+
+interface IUpdateProfilePicture {
+	avatar: string;
+}
+// *** update profile picture *** //
+export const updateProfilePicture = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { avatar }: IUpdateProfilePicture = req.body as IUpdateProfilePicture;
+
+		if (!avatar) {
+			return next(new ErrorHandler('Please provide an avatar', 400));
+		}
+
+		const userId = req.user?._id;
+
+		const user: IUser = (await User.findById(userId)) as IUser;
+		if (!user) {
+			return next(new ErrorHandler('User not found', 404));
+		}
+
+		// if user already has an avatar, delete it
+		if (user.avatar.public_id) {
+			await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+		}
+
+		// then upload new avatar and save it to user
+		const myCloudinary = await cloudinary.v2.uploader.upload(avatar, {
+			folder: 'avatars',
+			width: 150,
+		});
+
+		user.avatar = {
+			public_id: myCloudinary.public_id,
+			url: myCloudinary.secure_url,
+		};
+
+		await user.save();
 		await redis.set(userId || '', JSON.stringify(user));
 
 		res.status(200).json({
