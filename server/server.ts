a8a16860e41cc26959d36ade9e2896c302ceb9ed
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-21 22:39:46 +0300

    connect with cloudinary

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,14 @@
+import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 import { app } from './app';
 import connectDB from './utils/db';
+
+// cloudinary config
+cloudinary.config({
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUD_API_KEY,
+	api_secret: process.env.CLOUD_API_SECRET_KEY,
+});
 
 process.on('uncaughtException', (err) => {
 	console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
