9c6f662c3f339725b488a8e9f9433e39df6a7270
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-23 19:42:06 +0300

    add "courses" to REST APIs routes

--- server/routes/index.route.ts
+++ server/routes/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import courseRoutes from './APIs/course.route';
 import userRoutes from './APIs/user.route';
 
 const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/courses', courseRoutes);
 
 export default router;
