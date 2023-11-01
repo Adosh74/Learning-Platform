e3cee18cf648ac77862d54799e3f810e27f5d82e
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-02 00:57:30 +0200

    create "update course" controller

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -26,3 +26,47 @@ export const uploadCourse = catchAsync(
 		createCourse(data, res, next);
 	}
 );
+
+// *** update course *** //
+export const updateCourse = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const data = req.body;
+
+		// if user upload new thumbnail
+		const thumbnail = data.thumbnail;
+
+		if (thumbnail) {
+			await cloudinary.v2.uploader.destroy(data.thumbnail.public_id);
+			const uploadResponse = await cloudinary.v2.uploader.upload(thumbnail, {
+				folder: 'courses',
+				quality: 'auto',
+				fetch_format: 'auto',
+			});
+
+			data.thumbnail = {
+				public_id: uploadResponse.public_id,
+				url: uploadResponse.secure_url,
+			};
+		}
+
+		// find course and update
+		const courseId = req.params.id;
+		const course = await Course.findByIdAndUpdate(
+			courseId,
+			{ $set: data },
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
+
+		if (!course) {
+			return next(new ErrorHandler('Course not found', 404));
+		}
+
+		res.status(200).json({
+			success: true,
+			course,
+		});
+	}
+);
