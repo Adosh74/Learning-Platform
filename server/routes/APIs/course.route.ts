6468edeae14a5ee241327ea3e13e0edd13e15c3a
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-02 00:45:20 +0200

    add "create course" route

--- server/routes/APIs/course.route.ts
+++ server/routes/APIs/course.route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import * as courseController from '../../controllers/course.controller';
+import { isAuthenticated, authorizeRoles } from '../../middleware/auth';
 
 const router = Router();
 
-router.route('/').post(courseController.uploadCourse);
+router
+	.route('/')
+	.post(isAuthenticated, authorizeRoles('admin'), courseController.uploadCourse);
 
 export default router;
