62b8db0265b387ddaab04e9541b4ffa64874e3ae
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-02 00:44:11 +0200

    update error middleware

--- server/middleware/error.ts
+++ server/middleware/error.ts
@@ -7,6 +7,7 @@ export const ErrorMiddleware = (
 	res: Response,
 	next: NextFunction
 ) => {
+	console.log(err);
 	err.statusCode = err.statusCode || 500;
 	err.message = err.message || 'Internal Server Error';
 	if (process.env.NODE_ENV === 'development') {
